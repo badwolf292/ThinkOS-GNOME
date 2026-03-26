91244baca336019864a379fe97354b0e13aeaf46
badwolf292 <misc@ersdalolsen.no>
2026-03-26 15:39:13 +0100

    Remove some packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -21,7 +21,7 @@ EOF
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux rclone android-tools nmap jotta-cli 
+dnf5 install -y android-tools jotta-cli 
 
 # Use a COPR Example:
 #
